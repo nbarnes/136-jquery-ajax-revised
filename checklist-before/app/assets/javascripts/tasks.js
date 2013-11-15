84b776e032bfc881a6bd50d6c25fccfa8b7c2987
NBarnes <nbarnes@gmail.com>
2013-11-15 15:15:18 -0800

    AJAX-ification complete per assignment

---
+++ checklist-before/app/assets/javascripts/tasks.js
@@ -0,0 +1,18 @@
+$(function () {
+  $('.edit_task input[type=submit]').remove();
+  $('.edit_task input[type=checkbox]').click(function () {
+    $(this).parent('form').submit();
+  });
+});
+
+$(function () {
+  $(".edit_task").submitOnCheck();
+});
+
+jQuery.fn.submitOnCheck = function () {
+  this.find('input[type=submit]').remove();
+  this.find('input[type=checkbox]').click(function () {
+    $(this).parent('form').submit();
+  });
+  return this;
+};
